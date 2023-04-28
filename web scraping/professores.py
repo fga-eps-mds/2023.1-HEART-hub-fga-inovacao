28257562efebff449678d930d14b7b9a331c5dda
Izabella Alves <iap21010705@gmail.com>
2023-04-27 21:56:20 -0300

    acessando cada prof
    eh possivel acessar a pagina pessoal de cada prof, porém faltam informações de alguns e eh preciso tratar isso

--- web scraping/professores.py
+++ web scraping/professores.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+linkprofs = []
+
 url = "https://fga.unb.br/professores"
 response = requests.get(url)
 
@@ -11,4 +13,34 @@ ul_list = soup.find_all("div", class_="common-profile-list-block")
 for ul in ul_list:
     nome = ul.find("a", class_="profile_link url")
     href = nome["href"]
-    print(href)
+    linkprofs.append(href) 
+
+#print(linkprofs)
+"""
+for profurl in linkprofs:
+    response2 = requests.get(profurl)
+
+    sp = BeautifulSoup(response2.content, "html.parser")
+    #print(sp)
+
+    prof = sp.find_all("div", class_="article-body article-body-text-article")
+    
+    if len(prof) > 0:
+        nome2 = prof[0].find_all("p", class_="MsoNormal")
+        
+        for p in nome2:
+            print(p.text)
+    else:
+        print(f"Não foi encontrada nenhuma informação deste professor {profurl}")
+"""
+profurl = linkprofs[2]
+response2 = requests.get(profurl)
+
+sp = BeautifulSoup(response2.content, "html.parser")
+    #print(sp)
+
+prof = sp.find_all("div", class_="article-body article-body-text-article")
+nome2 = prof[0].find_all("p", class_="MsoNormal")
+nome3 = nome2[0].find_all("span")
+print(nome3[1].text)
+print(nome2[1].text)

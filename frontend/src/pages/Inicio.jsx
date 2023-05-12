fd11120c25ecfeb6e7dc18ce26c53af9be1756e3
Izabella Alves <iap21010705@gmail.com>
2023-05-12 19:08:57 -0300

    conflitos

--- frontend/src/pages/Inicio.jsx
+++ frontend/src/pages/Inicio.jsx
@@ -1,11 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import { Carousel } from 'react-bootstrap';
-import { motion } from 'framer-motion';
-import img1 from './../img/img1.png';
-import img2 from './../img/img2.png';
-import img3 from './../img/img3.png';
-import img4 from './../img/img4.png';
-import img5 from './../img/img5.png';
+import React from 'react';
 import Sec1 from '../components/Sec1';
 
 

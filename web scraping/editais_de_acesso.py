3f1e023578f3d11dd25d4fd64406e97e09dced79
Mariiana S. Neris <98031097+Maryyscreuza@users.noreply.github.com>
2023-05-26 10:55:57 -0300

    Adicionando itens editais_de_acesso.py

--- web scraping/editais_de_acesso.py
+++ web scraping/editais_de_acesso.py
@@ -1,1 +1,34 @@
+import json
+
+editais = [
+    {
+        "nome": "ACESSO PAS - UnB",
+        "descrição": "O Programa de Avaliação Seriada (PAS) é um processo seletivo da Universidade de Brasília (UnB), realizado ao longo dos três anos do ensino médio regular. Atualmente, a Universidade destina a metade das vagas em todos os seus cursos aos aprovados no Programa, abrindo as portas da Instituição para os estudantes do ensino médio de forma gradual e progressiva.",
+        "ano do edital": "Subprograma 2022-2024",
+        "edital": "https://cdn.cebraspe.org.br/pas/arquivos/ED_1_PAS_1_2022-2024_abertura.pdf",
+        "edital em Libras": "https://security.cebraspe.org.br/PAS_2022_1/EditaisLibras/Editallibras_abertura/"
+        },
+    {
+        "nome": "ACESSO Vestibular - UnB",
+        "descrição": "O acesso vestibular à Universidade de Brasília (UnB) é uma das formas de ingresso para os cursos de graduação oferecidos pela instituição.  o acesso vestibular é realizado por meio de um processo seletivo que ocorre anualmente. Os candidatos interessados devem se inscrever para participar do vestibular da UnB, que é composto por provas abrangendo as diferentes áreas de conhecimento.",
+        "ano do edital": "2023",
+        "edital": "https://cdn.cebraspe.org.br/vestibulares/VESTUNB_23/arquivos/ED_1_2023_UNB_VEST_TRADICIONAL_E_LIBRAS_ABERTURA.PDF",
+        "edital em Libras": "https://security.cebraspe.org.br/VESTUNB_23/EditaisLibras/Editallibras_abertura/"
+        },
+    {
+        "nome": "ACESSO ENEM - UnB",
+        "descrição": "É possível ingressar na Faculdade UnB Gama (FGA) por meio do Exame Nacional do Ensino Médio (Enem). A UnB utiliza o Sistema de Seleção Unificada (SiSU) como forma de acesso para seus cursos de graduação, incluindo a FGA.",
+        "ano do edital": "2023",
+        "edital": "https://cdn.cebraspe.org.br/vestibulares/UNB_23_ACESSOENEM/arquivos/ED_1_2023_ACESSO_ENEM_ABERTURA.PDF",
+        "edital em Libras": "https://security.cebraspe.org.br/UNB_23_ACESSOENEM/EditaisLibras_3102E084-49EA-4AED-B389/Editallibras_abertura/"
+        },
+    {
+        "nome": "Transferência Facultativa (TF)",
+        "descrição": "Forma de ingresso na UnB, mediante processo seletivo, para alunos regulares de outras Instituições de Ensino Superior (IES), públicas ou particulares, nacionais ou estrangeiras, para prosseguimento de estudos no mesmo curso ou em curso equivalente, visando ao preenchimento de vagas de graduação ociosas da Universidade. Entende-se como curso equivalente aquele que confira o mesmo grau de formação e tenha carga horária compatível com o curso pretendido na UnB.",
+        "ano do edital": "2023",
+        "edital": "https://www.iades.com.br/inscricao/upload/319/20230104102927961.pdf",
+        "edital em Libras": "https://www.youtube.com/playlist?list=PLvH8tt8u9y9aA9YiZGXTYyvlohcfJjwgL"
+        },
+
+]
 

a9bd0bc38ae3a71056e79b65740d00170aae3f85
Lucas13032003 <lucasvictor.lvfa18@gmail.com>
2023-05-27 19:49:36 -0300

    footer_equipe-competicoes

--- frontend/src/pages/EquipesdeCompeticao.jsx
+++ frontend/src/pages/EquipesdeCompeticao.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import './EquipesdeCompeticao.css'   
+import './EquipesdeCompeticao.css';
+import img_logo from '../img/logo-unb.png';
+import img_fga from '../img/fga.png';    
 import { FiGithub,
   FiFigma,
   FiLinkedin,
@@ -15,40 +17,49 @@ const EquipesdeCompeticao = () => {
                 </div>
             </section>
             <div class="footer-basic">
-                <footer>
-                    <div class="social">
-                      <a href="https://github.com/fga-eps-mds/2023.1-HEART-hub-fga-inovacao">
-                        <i class="icon ion-social-instagram">
-                          <FiGithub/>
-                        </i>
-                      </a>
-                      <a href="#">
-                        <i class="icon ion-social-snapchat">
-                          <FiLinkedin/>
-                        </i>
-                      </a>
-                      <a href="https://www.figma.com/file/oiha0mXD5eHoqBSWUEPlPR/Heart---HUB-FGA-Inova%C3%A7%C3%A3o?type=design&node-id=0%3A1&t=7mEIQJVT3hkgRnyL-1">
-                        <i class="icon ion-social-twitter">
-                          <FiFigma/>
-                        </i>
-                      </a>
-                      <a href="#">
-                        <i class="icon ion-social-facebook">
-                          <FiInstagram/>
-                        </i>
-                      </a>
-                    </div>
-                    <ul class="list-inline">
-                        <li class="list-inline-item"><a href="pedei">P&D&I</a></li>
-                        <li class="list-inline-item"><a href="professores">Professores</a></li>
-                        <li class="list-inline-item"><a href="empresasjuniores">Empresas Juniores</a></li>
-                        <li class="list-inline-item"><a href="equipesdecompetição">Equipes de Competição</a></li>
-                        <li class="list-inline-item"><a href="disciplinas">Disciplinas</a></li>
-                        <li class="list-inline-item"><a href="editais">Editais</a></li>
-                        <li class="list-inline-item"><a href="contatos">Contatos</a></li>
-                    </ul>
-                    <p class="copyright">Heart - HUB FGA Inovação © 2023</p>
-                </footer>
+            <footer>
+            <div className="social">
+              <a href="https://github.com/fga-eps-mds/2023.1-HEART-hub-fga-inovacao">
+                <i className="icon ion-social-instagram">
+                  <FiGithub/>
+                </i>
+              </a>
+              <a href="#">
+                <i className="icon ion-social-snapchat">
+                  <FiLinkedin/>
+                </i>
+              </a>
+              <a href="https://www.figma.com/file/oiha0mXD5eHoqBSWUEPlPR/Heart---HUB-FGA-Inova%C3%A7%C3%A3o?type=design&node-id=0%3A1&t=7mEIQJVT3hkgRnyL-1">
+                <i className="icon ion-social-twitter">
+                  <FiFigma/>
+                </i>
+              </a>
+              <a href="#">
+                <i className="icon ion-social-facebook">
+                  <FiInstagram/>
+                </i>
+              </a>
+            <p className='texto_footer'>Este HUB é referente à Universidade de Brasília - Campus Gama</p>
+            </div>
+              <ul className="list-inline">
+                <li className="list-inline-item"><a href="pedei">P&D&I</a></li>
+                <li className="list-inline-item"><a href="professores">Professores</a></li>
+                <li className="list-inline-item"><a href="empresasjuniores">Empresas Juniores</a></li>
+                <li className="list-inline-item"><a href="equipesdecompetição">Equipes de Competição</a></li>
+                <li className="list-inline-item"><a href="disciplinas">Disciplinas</a></li>
+                <li className="list-inline-item"><a href="editais">Editais</a></li>
+                <li className="list-inline-item"><a href="contatos">Contatos</a></li>
+              </ul>
+            <p className="copyright">Heart - HUB FGA Inovação © 2023</p>
+            <div className='logo_unb'>
+            <a href="https://www.unb.br/">
+              <img className="img_logo" src={img_logo} alt="logo"/>
+            </a>
+            <a href="https://fga.unb.br/">
+              <img className='fga' src={img_fga} alt='fga'/>
+            </a>
+            </div>
+          </footer>
             </div>
         </section>
     );

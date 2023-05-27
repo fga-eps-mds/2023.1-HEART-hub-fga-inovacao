33e7bd73c1592c974f02eb3378f1862aa692afa5
Lucas Oliveira <lucasebj@gmail.com>
2023-05-27 19:36:07 -0300

    secção1 contatos

--- frontend/src/pages/Contatos.jsx
+++ frontend/src/pages/Contatos.jsx
@@ -6,75 +6,127 @@ import { FiGithub,
   FiFigma,
   FiLinkedin,
   FiInstagram,
-  FiPhone
+  FiPhone,
+  FiMapPin,
+  FiClock,
+  FiMail,
+  FiTwitter,
+  FiFacebook
 } from "react-icons/fi";
+{/*Importação de todos os componentes e estilos necessários para esta página*/}
 
 const Contatos = () => {
-    return (
-        <section className='section'>
-            <section className='first-section'>
-                <div>
-                    <h1 className='contatos'></h1>
-                    <div className="card-container">
+  return (
+    <section className='section'>
+      <section className='first-section'>
+        <div>
+          <h1 className='contatos'></h1>
+          <div className="card-container">
+{/*Definição do componente contatos, aqui ele está retornando a estrutura da pagina*/}
+
+          <Card
+              title={
+                <>
+                  <div className="title">
+                  <FiPhone className='iconc1'/> <a href='https://fga.unb.br/unb-gama/contato'>
+                  Ligue para nós
+                    <p className='iconp1'>+55 (61) 3107-8901/ +55 (61) 3107-8907 </p> 
+                  </a>
+                  </div>
+                  <br></br>
+
+                  <div className="title">
+                   <FiMapPin  className='iconc1' /><a href='https://abre.ai/geFp'> Localização
+                    <p className='iconp1'>St.Leste Projeção A- Gama Leste, <br></br>
+                      Brasília-DF,72444-240</p>
+                      </a>
+                  </div>
+
+                  <br></br>
+
+                  <div className="title">
+                  <FiClock className='iconc1'/> <a href='https://fga.unb.br/unb-gama/contato'>Horário comercial
+                    <p className='iconp1'>Seg-Sex: 10 horas - 18 horas</p>
+                    </a>
+                  </div>
+                </>
+              }
+              description=""
+              color="#8E4694"
+              />
+{/*Acima temos o primeiro componente card criado, que tem as propriedades de
+titulo,descrção e cor, nesse card temos, cada secção tem um icone, um link e 
+um parágrafo de texto descritivo*/}
+
             <Card 
-            title="Meios de contato" 
-            description="" 
-            color="#8E4694"
-            icon={<FiPhone/>} // Adicione o ícone desejado aqui
-                    />
+              title="Card 2" 
+              description="" 
+              color="#f5f5f5" 
+              newTitle="Entre em Contato"
+            />
+{/*Acima temos o segundo componete, que é o card do meio, a função dele é conter
+o formulário para envio de mensagens para o site */}
+
             <Card 
-            title="Card 2" 
-            description= "" 
-            color="#f5f5f5" 
-            newTitle="Entre em Contato"/>
-             
-            <Card 
-            title="Redes Sociais" 
-            description="" 
-            color="#8E4694"/>
-        </div>
+              title="" 
+              description="" 
+              color="#8E4694"
+              icon={
+                <>
+                  <div className='iconc3'>
+                    <a href='https://github.com/fga-eps-mds/2023.1-HEART-hub-fga-inovacao'>
+                    <FiGithub/>
+                    </a>
+                  </div>
                     
-                </div>
-                
-            </section>
-            <div class="footer-basic">
-                <footer>
-                    <div class="social">
-                      <a href="https://github.com/fga-eps-mds/2023.1-HEART-hub-fga-inovacao">
-                        <i class="icon ion-social-instagram">
-                          <FiGithub/>
-                        </i>
-                      </a>
-                      <a href="#">
-                        <i class="icon ion-social-snapchat">
-                          <FiLinkedin/>
-                        </i>
-                      </a>
-                      <a href="https://www.figma.com/file/oiha0mXD5eHoqBSWUEPlPR/Heart---HUB-FGA-Inova%C3%A7%C3%A3o?type=design&node-id=0%3A1&t=7mEIQJVT3hkgRnyL-1">
-                        <i class="icon ion-social-twitter">
-                          <FiFigma/>
-                        </i>
-                      </a>
-                      <a href="#">
-                        <i class="icon ion-social-facebook">
-                          <FiInstagram/>
-                        </i>
-                      </a>
-                    </div>
-                    <ul class="list-inline">
-                        <li class="list-inline-item"><a href="pedei">P&D&I</a></li>
-                        <li class="list-inline-item"><a href="professores">Professores</a></li>
-                        <li class="list-inline-item"><a href="empresasjuniores">Empresas Juniores</a></li>
-                        <li class="list-inline-item"><a href="equipesdecompetição">Equipes de Competição</a></li>
-                        <li class="list-inline-item"><a href="disciplinas">Disciplinas</a></li>
-                        <li class="list-inline-item"><a href="editais">Editais</a></li>
-                        <li class="list-inline-item"><a href="contatos">Contatos</a></li>
-                    </ul>
-                    <p class="copyright">Heart - HUB FGA Inovação © 2023</p>
-                </footer>
-            </div>
-        </section>
-    );
+                  <br></br>
+
+                  <div className='iconc3'>
+                    <a href='https://instagram.com/unb_oficial?igshid=MzRlODBiNWFlZA=='>
+                    <FiInstagram />
+                    </a>
+                  </div>
+                  <br></br>
+
+                  <div className='iconc3'>
+                    <a href='https://www.linkedin.com/school/universidade-de-bras-lia/'>
+                    <FiLinkedin /> 
+                    </a>
+                  </div>
+                  <br></br>
+                  
+                  <div className='iconc3'>
+                    <a href='https://fga.unb.br/unb-gama/contato'>
+                    <FiMail /> 
+                    </a>
+                  </div>
+                  <br></br>
+                    
+                  <div className='iconc3'>
+                    <a href='https://twitter.com/unb_oficial'>
+                    <FiTwitter /> 
+                    </a>
+                  </div>
+                  <br></br>
+                      
+                  <div className='iconc3'>
+                    <a href='https://m.facebook.com/unbgama'>
+                    <FiFacebook/>
+                    </a>
+                  </div>
+                </>
+              }
+            />
+{/*Acima temos o terceiro e ultimo card desta secção que tem varios icones 
+renderizados como links clicaveis, cada icone representa um link para uma
+rede social diferente*/}
+
+          </div>
+        </div>
+      </section>
+      {/* Resto do código */}
+    </section>
+  );
 };
 
 export default Contatos;

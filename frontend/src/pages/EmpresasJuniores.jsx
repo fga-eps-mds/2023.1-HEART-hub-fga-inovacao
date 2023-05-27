9781097eb910e02c4a27d49a1bdf714230259d75
Lucas13032003 <lucasvictor.lvfa18@gmail.com>
2023-05-27 20:07:08 -0300

    footer_disciplina

--- frontend/src/pages/EmpresasJuniores.jsx
+++ frontend/src/pages/EmpresasJuniores.jsx
@@ -1,6 +1,13 @@
 import React, { useEffect, useState, useRef } from 'react';
-import './EmpresasJuniores.css'   
+import './EmpresasJuniores.css';
+import img_logo from '../img/logo-unb.png';
+import img_fga from '../img/fga.png';   
 import { FiChevronRight } from "react-icons/fi";
+import { FiGithub,
+    FiFigma,
+    FiLinkedin,
+    FiInstagram
+  } from "react-icons/fi";
 
 function EmpresasJuniores() {
     const [data, setData] = useState([]);
@@ -52,6 +59,49 @@ function EmpresasJuniores() {
                     <FiChevronRight className='btn'/>
                 </button>
             </div>
+            <footer>
+            <div className="social">
+              <a href="https://github.com/fga-eps-mds/2023.1-HEART-hub-fga-inovacao">
+                <i className="icon ion-social-instagram">
+                  <FiGithub/>
+                </i>
+              </a>
+              <a href="#">
+                <i className="icon ion-social-snapchat">
+                  <FiLinkedin/>
+                </i>
+              </a>
+              <a href="https://www.figma.com/file/oiha0mXD5eHoqBSWUEPlPR/Heart---HUB-FGA-Inova%C3%A7%C3%A3o?type=design&node-id=0%3A1&t=7mEIQJVT3hkgRnyL-1">
+                <i className="icon ion-social-twitter">
+                  <FiFigma/>
+                </i>
+              </a>
+              <a href="#">
+                <i className="icon ion-social-facebook">
+                  <FiInstagram/>
+                </i>
+              </a>
+            <p className='texto_footer'>Este HUB é referente à Universidade de Brasília - Campus Gama</p>
+            </div>
+              <ul className="list-inline">
+                <li className="list-inline-item"><a href="pedei">P&D&I</a></li>
+                <li className="list-inline-item"><a href="professores">Professores</a></li>
+                <li className="list-inline-item"><a href="empresasjuniores">Empresas Juniores</a></li>
+                <li className="list-inline-item"><a href="equipesdecompetição">Equipes de Competição</a></li>
+                <li className="list-inline-item"><a href="disciplinas">Disciplinas</a></li>
+                <li className="list-inline-item"><a href="editais">Editais</a></li>
+                <li className="list-inline-item"><a href="contatos">Contatos</a></li>
+              </ul>
+            <p className="copyright">Heart - HUB FGA Inovação © 2023</p>
+            <div className='logo_unb'>
+            <a href="https://www.unb.br/">
+              <img className="img_logo" src={img_logo} alt="logo"/>
+            </a>
+            <a href="https://fga.unb.br/">
+              <img className='fga' src={img_fga} alt='fga'/>
+            </a>
+            </div>
+          </footer>
         </section>
     );
 };

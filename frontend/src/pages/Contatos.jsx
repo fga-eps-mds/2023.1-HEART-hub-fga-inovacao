4b43dae6ba657f16d6d3e7651dda77033a4fb250
Esther Sena <211062769@aluno.unb.br>
2023-05-27 21:59:55 -0300

    tela contato

--- frontend/src/pages/Contatos.jsx
+++ frontend/src/pages/Contatos.jsx
@@ -237,9 +237,45 @@ rede social diferente*/}
 
             </section>
             
-          
 
-            
+            <section>
+            <div class="footer-basic">
+                <footer>
+                    <div class="social">
+                      <a href="https://github.com/fga-eps-mds/2023.1-HEART-hub-fga-inovacao">
+                        <i class="icon ion-social-instagram">
+                          <FiGithub/>
+                        </i>
+                      </a>
+                      <a href="#">
+                        <i class="icon ion-social-snapchat">
+                          <FiLinkedin/>
+                        </i>
+                      </a>
+                      <a href="https://www.figma.com/file/oiha0mXD5eHoqBSWUEPlPR/Heart---HUB-FGA-Inova%C3%A7%C3%A3o?type=design&node-id=0%3A1&t=7mEIQJVT3hkgRnyL-1">
+                        <i class="icon ion-social-twitter">
+                          <FiFigma/>
+                        </i>
+                      </a>
+                      <a href="#">
+                        <i class="icon ion-social-facebook">
+                          <FiInstagram/>
+                        </i>
+                      </a>
+                    </div>
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><a href="pedei">P&D&I</a></li>
+                        <li class="list-inline-item"><a href="professores">Professores</a></li>
+                        <li class="list-inline-item"><a href="empresasjuniores">Empresas Juniores</a></li>
+                        <li class="list-inline-item"><a href="equipesdecompetição">Equipes de Competição</a></li>
+                        <li class="list-inline-item"><a href="disciplinas">Disciplinas</a></li>
+                        <li class="list-inline-item"><a href="editais">Editais</a></li>
+                        <li class="list-inline-item"><a href="contatos">Contatos</a></li>
+                    </ul>
+                    <p class="copyright">Heart - HUB FGA Inovação © 2023</p>
+                </footer>
+            </div>
+    </section>
     </section>
     );
   };

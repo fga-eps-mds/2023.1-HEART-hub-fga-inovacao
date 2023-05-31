1b1b1ff5e1c5732c050c34e4131fc44a55d2f2c7
Izabella Alves <iap21010705@gmail.com>
2023-05-31 15:05:42 -0300

    resoluçao de pequeno problema

--- frontend/src/pages/Contatos.jsx
+++ frontend/src/pages/Contatos.jsx
@@ -3,7 +3,7 @@ import './Contatos.css'
 import Card from './Card';
 import img_01 from '../img/grupo.jpeg';
 import img_02 from '../img/fotoIzabella.jpeg';
-import img_03 from '../img/fotEsther.jpeg';
+import img_03 from '../img/fotoEsther.jpeg';
 
 import { FiGithub,
   FiUsers,
@@ -22,48 +22,41 @@ import { FiGithub,
 const Contatos = () => {
   return (
     <section className='section'>
-
       <section className='first-section'>
         <div>
           <h1 className='contatos'></h1>
           <div className="card-container">
-{/*Definição do componente contatos, aqui ele está retornando a estrutura da pagina*/}
-
-          <Card
+            <Card
               title={
                 <>
                   <div className="title">
-                  <FiPhone className='iconc1'/> <a href='https://fga.unb.br/unb-gama/contato'>
-                  Ligue para nós
-                    <p className='iconp1'>+55 (61) 3107-8901/ +55 (61) 3107-8907 </p> 
-                  </a>
+                    <FiPhone className='iconc1'/> <a href='https://fga.unb.br/unb-gama/contato'>
+                      Ligue para nós
+                      <p className='iconp1'>+55 (61) 3107-8901/ +55 (61) 3107-8907 </p> 
+                    </a>
                   </div>
-                  <br></br>
+                  <br />
 
                   <div className="title">
-                   <FiMapPin  className='iconc1' /><a href='https://abre.ai/geFp'> Localização
-                    <p className='iconp1'>St.Leste Projeção A- Gama Leste, <br></br>
-                      Brasília-DF,72444-240</p>
-                      </a>
+                    <FiMapPin className='iconc1' /><a href='https://abre.ai/geFp'> Localização
+                      <p className='iconp1'>St.Leste Projeção A- Gama Leste, <br />
+                        Brasília-DF,72444-240</p>
+                    </a>
                   </div>
 
-                  <br></br>
+                  <br />
 
                   <div className="title">
-                  <FiClock className='iconc1'/> <a href='https://fga.unb.br/unb-gama/contato'>Horário comercial
-                    <p className='iconp1'>Seg-Sex: 08 horas - 18 horas <br></br>
-                    e 14 horas-18 horas</p>
-
+                    <FiClock className='iconc1'/> <a href='https://fga.unb.br/unb-gama/contato'>Horário comercial
+                      <p className='iconp1'>Seg-Sex: 08 horas - 18 horas <br />
+                      e 14 horas-18 horas</p>
                     </a>
                   </div>
                 </>
               }
               description=""
               color="#8E4694"
-              />
-{/*Acima temos o primeiro componente card criado, que tem as propriedades de
-titulo,descrção e cor, nesse card temos, cada secção tem um icone, um link e 
-um parágrafo de texto descritivo*/}
+            />
 
             <Card 
               title="Card 2" 
@@ -71,8 +64,6 @@ um parágrafo de texto descritivo*/}
               color="#f5f5f5" 
               newTitle="Entre em Contato"
             />
-{/*Acima temos o segundo componete, que é o card do meio, a função dele é conter
-o formulário para envio de mensagens para o site */}
 
             <Card 
               title="" 
@@ -82,212 +73,83 @@ o formulário para envio de mensagens para o site */}
                 <>
                   <div className='iconc3'>
                     <a href='https://github.com/fga-eps-mds/2023.1-HEART-hub-fga-inovacao'>
-                    <FiGithub/>
+                      <FiGithub/>
                     </a>
                   </div>
                     
-                  <br></br>
+                  <br />
 
                   <div className='iconc3'>
                     <a href='https://instagram.com/unb_oficial?igshid=MzRlODBiNWFlZA=='>
-                    <FiInstagram />
+                      <FiInstagram />
                     </a>
                   </div>
-                  <br></br>
+                  <br />
 
                   <div className='iconc3'>
                     <a href='https://www.linkedin.com/school/universidade-de-bras-lia/'>
-                    <FiLinkedin /> 
+                      <FiLinkedin /> 
                     </a>
                   </div>
-                  <br></br>
+                  <br />
                   
                   <div className='iconc3'>
                     <a href='https://fga.unb.br/unb-gama/contato'>
-                    <FiMail /> 
+                      <FiMail /> 
                     </a>
                   </div>
-                  <br></br>
+                  <br />
                     
                   <div className='iconc3'>
                     <a href='https://twitter.com/unb_oficial'>
-                    <FiTwitter /> 
+                      <FiTwitter /> 
                     </a>
                   </div>
-                  <br></br>
+                  <br />
                       
                   <div className='iconc3'>
                     <a href='https://m.facebook.com/unbgama'>
-                    <FiFacebook/>
+                      <FiFacebook/>
                     </a>
                   </div>
                 </>
               }
             />
-{/*Acima temos o terceiro e ultimo card desta secção que tem varios icones 
-renderizados como links clicaveis, cada icone representa um link para uma
-rede social diferente*/}
-
           </div>
         </div>
       </section>
-      {/* Resto do código */}
-    </section>
-  );
+
+      <section className='second-section'>
+        <h1 className='missao'>Nossa Equipe</h1>
+        <div className="team-container">
+          <div className="team-member">
+            <div className="member-content">
+            <img className="member-image" src="img/membro1.jpg" alt="Membro 1" />
+
+<h3 className="member-name">Membro 1</h3>
+<p className="member-role">Cargo do Membro 1</p>
+</div>
+</div>
+
+<div className="team-member">
+        <div className="member-content">
+          <img className="member-image" src="img/membro2.jpg" alt="Membro 2" />
+          <h3 className="member-name">Membro 2</h3>
+          <p className="member-role">Cargo do Membro 2</p>
+        </div>
+      </div>
+
+      <div className="team-member">
+        <div className="member-content">
+          <img className="member-image" src="img/membro3.jpg" alt="Membro 3" />
+          <h3 className="member-name">Membro 3</h3>
+          <p className="member-role">Cargo do Membro 3</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</section>
+);
 };
-=======
-  
-
-            <section className='second-section'>
-            <h1 className='missao'>Nossa Equipe</h1>
-
-            <div class="team-container">
-          <div class="team-member">
-          <div class="member-content">
-          <img className="img_02" src={img_02} alt="contato"/>
-          <div class="member-details">
-            <h3>Esther Sena</h3>
-            <div class="social-media">
-            <a href="https://www.linkedin.com/in/esther-sena-martins-003b021b3/"><i className="fab fa-linkedin"><FiLinkedin/></i></a>
-              <a href="https://br.sonychannel.com/home" ><i className="fab fa-linkedin"><FiUsers/></i></a>
-              <a href="https://github.com/esmsena" target="_blank"> <i className="fab fa-github"><FiGithub/></i></a>
-            </div>
-            </div>
-          </div>
-          </div>
-
-          <div class="team-member">
-          <div class="member-content">
-          <img className="img_01" src={img_01} alt="contato"/>
-          <div class="member-details">
-            <h3>Mariiana Siqueira</h3>
-            <div class="social-media">
-              <a href="https://br.linkedin.com/"><i className="fab fa-linkedin"><FiLinkedin/></i></a>
-              <a href="https://br.sonychannel.com/home" ><i className="fab fa-linkedin"><FiUsers/></i></a>
-              <a href="https://github.com/fga-eps-mds/2023.1-HEART-hub-fga-inovacao" target="_blank"> <i className="fab fa-github"><FiGithub/></i></a>
-            </div>
-            </div>
-          </div>
-          </div>
-
-                  <div class="team-member">
-          <div class="member-content">
-          <img className="img_03" src={img_03} alt="contato"/>
-          <div class="member-details">
-            <h3>Izabella Alves</h3>
-            <div class="social-media">
-              <a href="https://br.linkedin.com/"><i className="fab fa-linkedin"><FiLinkedin/></i></a>
-              <a href="https://br.sonychannel.com/home" ><i className="fab fa-linkedin"><FiUsers/></i></a>
-              <a href="https://github.com/fga-eps-mds/2023.1-HEART-hub-fga-inovacao" target="_blank"> <i className="fab fa-github"><FiGithub/></i></a>
-            </div>
-            </div>
-          </div>
-          </div>
-
-
-                  <div class="team-member">
-          <div class="member-content">
-          <img className="img_01" src={img_01} alt="contato"/>
-          <div class="member-details">
-            <h3>Artur Ricardo Silva</h3>
-            <div class="social-media">
-              <a href="https://br.linkedin.com/"><i className="fab fa-linkedin"><FiLinkedin/></i></a>
-              <a href="https://br.sonychannel.com/home" ><i className="fab fa-linkedin"><FiUsers/></i></a>
-              <a href="https://github.com/fga-eps-mds/2023.1-HEART-hub-fga-inovacao" target="_blank"> <i className="fab fa-github"><FiGithub/></i></a>
-            </div>
-            </div>
-          </div>
-          </div>
-
-                  <div class="team-member">
-          <div class="member-content">
-          <img className="img_01" src={img_01} alt="contato"/>
-          <div class="member-details">
-            <h3>Lucas Oliveira</h3>
-            <div class="social-media">
-              <a href="https://br.linkedin.com/"><i className="fab fa-linkedin"><FiLinkedin/></i></a>
-              <a href="https://br.sonychannel.com/home" ><i className="fab fa-linkedin"><FiUsers/></i></a>
-              <a href="https://github.com/fga-eps-mds/2023.1-HEART-hub-fga-inovacao" target="_blank"> <i className="fab fa-github"><FiGithub/></i></a>
-            </div>
-            </div>
-          </div>
-          </div>
-
-                  <div class="team-member">
-          <div class="member-content">
-          <img className="img_01" src={img_01} alt="contato"/>
-          <div class="member-details">
-            <h3>Lucas Ribeiro</h3>
-            <div class="social-media">
-              <a href="https://br.linkedin.com/"><i className="linkedin"><FiLinkedin/></i></a>
-              <a href="https://br.sonychannel.com/home" ><i className="linkedin"><FiUsers/></i></a>
-              <a href="https://github.com/fga-eps-mds/2023.1-HEART-hub-fga-inovacao" target="_blank"> <i className="fab fa-github"><FiGithub/></i></a>
-            </div>
-            </div>
-          </div>
-          </div>
-
-          <div class="team-member">
-          <div class="member-content">
-          <img className="img_01" src={img_01} alt="contato"/>
-          <div class="member-details">
-            <h3>Lucas Victor Ferreira</h3>
-            <div class="social-media">
-              <a href="https://br.linkedin.com/"><i className="fab fa-linkedin"><FiLinkedin/></i></a>
-              <a href="https://br.sonychannel.com/home" ><i className="fab fa-linkedin"><FiUsers/></i></a>
-              <a href="https://github.com/fga-eps-mds/2023.1-HEART-hub-fga-inovacao" target="_blank"> <i className="fab fa-github"><FiGithub/></i></a>
-            </div>
-            </div>
-          </div>
-          </div>
-        
-  </div>
-
-
-            </section>
-            
-
-            <section>
-            <div class="footer-basic">
-                <footer>
-                    <div class="social">
-                      <a href="https://github.com/fga-eps-mds/2023.1-HEART-hub-fga-inovacao">
-                        <i class="icon ion-social-instagram">
-                          <FiGithub/>
-                        </i>
-                      </a>
-                      <a href="#">
-                        <i class="icon ion-social-snapchat">
-                          <FiLinkedin/>
-                        </i>
-                      </a>
-                      <a href="https://www.figma.com/file/oiha0mXD5eHoqBSWUEPlPR/Heart---HUB-FGA-Inova%C3%A7%C3%A3o?type=design&node-id=0%3A1&t=7mEIQJVT3hkgRnyL-1">
-                        <i class="icon ion-social-twitter">
-                          <FiFigma/>
-                        </i>
-                      </a>
-                      <a href="#">
-                        <i class="icon ion-social-facebook">
-                          <FiInstagram/>
-                        </i>
-                      </a>
-                    </div>
-                    <ul class="list-inline">
-                        <li class="list-inline-item"><a href="pedei">P&D&I</a></li>
-                        <li class="list-inline-item"><a href="professores">Professores</a></li>
-                        <li class="list-inline-item"><a href="empresasjuniores">Empresas Juniores</a></li>
-                        <li class="list-inline-item"><a href="equipesdecompetição">Equipes de Competição</a></li>
-                        <li class="list-inline-item"><a href="disciplinas">Disciplinas</a></li>
-                        <li class="list-inline-item"><a href="editais">Editais</a></li>
-                        <li class="list-inline-item"><a href="contatos">Contatos</a></li>
-                    </ul>
-                    <p class="copyright">Heart - HUB FGA Inovação © 2023</p>
-                </footer>
-            </div>
-    </section>
-    </section>
-    );
-  };
-
 
 export default Contatos;
